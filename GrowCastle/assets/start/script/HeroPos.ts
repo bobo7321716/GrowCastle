33993343f6a52dfeae10baa9505e024456a2a0f6
zhaoyaohua <1196112662@qq.com>
2024-10-21 13:28:20 +0800

    英雄升级逻辑

--- GrowCastle/assets/start/script/HeroPos.ts
+++ GrowCastle/assets/start/script/HeroPos.ts
@@ -1,7 +1,16 @@
+import { AbManager } from "../../homepage/script/common/asssetsBundle/AbManager";
 import { AudioManager } from "../../homepage/script/common/AudioManager";
+import BtnCol from "../../homepage/script/common/BtnCol";
+import { BundleName } from "../../homepage/script/common/BundleName";
 import { Global } from "../../homepage/script/common/Global";
+import { PlayerData } from "../../homepage/script/common/PlayerData";
 import { UIManager } from "../../homepage/script/common/UIManager";
 import { UiPath } from "../../homepage/script/common/UiPath";
+import { WorldEventManager } from "../../homepage/script/common/WorldEventManager";
+import HeroattributeConfig, { HeroattributeConfigMgr } from "../../homepage/script/config/HeroattributeConfig";
+import HeroConfig, { HeroConfigMgr } from "../../homepage/script/config/HeroConfig";
+import { ItemConfigMgr } from "../../homepage/script/config/ItemConfig";
+import DataManager from "../../homepage/script/manager/DataManager";
 import ChooseHeroUI from "../../part1/script/ChooseHeroUI";
 import FightManager from "./FightManager";
 import PosHand from "./PosHand";
@@ -18,8 +27,28 @@ export default class HeroPos extends cc.Component {
     @property(PosHand)
     posHand: PosHand = null;
 
+    @property(BtnCol)
+    btnCol: BtnCol = null;
+
+    @property(cc.Node)
+    upgradeNode: cc.Node = null;
+
+    @property(cc.Label)
+    titleLab: cc.Label = null;
+
+    @property(cc.Label)
+    costLab: cc.Label = null;
+
+    @property(cc.Sprite)
+    costSpr: cc.Sprite = null;
+
     private heroId: number = null;
     private index: number = null;
+    private attributeConfig: HeroattributeConfig = null;
+
+    protected onLoad(): void {
+        WorldEventManager.addListener(Global.EventEnum.RefreshPlayerInfo, this.refreshCost, this);
+    }
 
     init(heroId: number, index: number) {
         return new Promise((resolve, reject) => {
@@ -31,7 +60,30 @@ export default class HeroPos extends cc.Component {
                 this.heroBase.reset();
                 resolve(null);
             }
+            this.refreshCost();
         })
+    }
+
+    refreshCost() {
+        let info = PlayerData.ins.unlockHeroInfos.find(v => v.id == this.heroId);
+        if (!info) {
+            info = { id: this.heroId, lv: 1, atkPos: -1, isBuy: false };
+        }
+        let datas = DataManager.ins.get(HeroattributeConfigMgr).datas;
+        this.attributeConfig = datas.find(v => v.heroid == this.heroId && v.level == info.lv);
+        if (!this.attributeConfig) return;
+        let costItme = DataManager.ins.get(ItemConfigMgr).getDataById(this.attributeConfig.coin[0]);
+        if (!costItme) return;
+        let isEnough = PlayerData.ins.getItemNum(this.attributeConfig.coin[0]) >= this.attributeConfig.coin[1];
+        this.btnCol.setIsGray(!isEnough);
+        AbManager.loadBundleRes(BundleName.Assets, `texture/item/` + costItme.id, cc.SpriteFrame).then((res) => {
+            this.costSpr.spriteFrame = res;
+        })
+        this.costLab.string = this.attributeConfig.coin[1].toString();
+        let allConfig = DataManager.ins.get(HeroattributeConfigMgr).getAllConfig(this.heroId);
+        let isMax = info.lv >= allConfig[allConfig.length - 1].level;
+        this.titleLab.string = isMax ? "已满级" : "升级";
+        this.upgradeNode.active = !isMax;
     }
 
     heroClick() {
